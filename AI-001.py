4a3d944457ad15eaed05d954eae2d7f3f36a49dc
Himekii <benjohncotton@gmail.com>
2025-03-07 15:39:37 +0000

    DFS Refined

--- AI-001.py
+++ AI-001.py
@@ -55,8 +55,7 @@ def move(state):
     """
     by, bx, rows = state
     
-    
-    if bx < 2: #Blank space not on right
+    if bx < len(rows[0])-1: #Blank space not on right
         #Move Right
         temp_rows = [list(row) for row in rows]
         temp_rows[by][bx], temp_rows[by][bx+1] = temp_rows[by][bx+1], 0
@@ -68,7 +67,7 @@ def move(state):
         temp_rows[by][bx], temp_rows[by][bx-1] = temp_rows[by][bx-1], 0
         next_state = [by, bx-1, temp_rows]
         yield next_state
-    if by < 2: #Blank space not at bottom
+    if by < len(rows)-1: #Blank space not at bottom
         #Move Down
         temp_rows = [list(row) for row in rows]
         temp_rows[by][bx], temp_rows[by+1][bx] = temp_rows[by+1][bx], 0
@@ -82,6 +81,7 @@ def move(state):
         yield next_state
 
     return None
+
 
 
 def dfs_stack(state, goal):
@@ -102,14 +102,22 @@ def dfs_stack(state, goal):
     
     predecessor = {}
     prev_node = None
+    prev_node_copy = prev_node
     
     while stack:
+        if len(predecessor) == 22472:
+            print("STOP")
+        
+        
         current = stack[-1]
         
         key = current[0], current[1], tuple(tuple(row) for row in current[2])
         
-        if key not in predecessor:
-            predecessor[key] = prev_node
+        if prev_node == key:
+            prev_node = predecessor[prev_node]
+        
+        if key not in predecessor: #If node is unexplored
+            predecessor[key] = prev_node #Add the node to predecessor with its parent node
         else:
             stack.pop()
             continue
@@ -117,18 +125,37 @@ def dfs_stack(state, goal):
         if current == goal:
             return get_path(key, predecessor)
 
-        offspring = move(current)
-
-        for next_state in offspring:
+        for next_state in move(current):
             stack.append(next_state)
-            
+        
+        
         prev_node = key
     print("No solution found")
 
 def iddfs_stack(state, goal):
     return 0;
 
+def manhatten():
+    return 0;
+
 def ida_stack(state, goal):
+    """
+    How to apply Iterative Deepening to A*?
+    1. A pass consists in expanding all nodes that have an value less
+        than or equal to a bound (threshold).
+    2. The initial bound is the value of the start state.
+    3. Expand all nodes that have an value less than or equal to the
+        actual bound (threshold).
+    4. If the goal is not found, update the bound,
+        the next bound is the smallest value of the states generated in
+        the search but not expanded.
+    5. Repeat (do another pass) until the goal is reached.
+    6. When the bound is updated, the search restarts from the start
+        state (ID).
+    """
+    
+    
+    
     return 0;
 
 print("\nDFS STACK:")
