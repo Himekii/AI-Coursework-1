7613e915423eba1fd79ac5b64ddcd4f753c7c2de
Himekii <benjohncotton@gmail.com>
2025-03-14 13:11:55 +0000

    Complete

--- IDAstar.py
+++ IDAstar.py
@@ -1,5 +1,68 @@
 import time
 
+"""
+State: 1
+Path Length: 20
+States accessed: 2757
+Computation Time: 24.003028869628906ms 
+
+
+State: 2
+Path Length: 22
+States accessed: 4020
+Computation Time: 33.9961051940918ms 
+
+
+State: 3
+Path Length: 26
+States accessed: 13844
+Computation Time: 167.7267551422119ms 
+
+
+State: 4
+Path Length: 30
+States accessed: 49335
+Computation Time: 468.0652618408203ms 
+
+
+State: 5
+Path Length: 22
+States accessed: 4299
+Computation Time: 35.9952449798584ms 
+
+
+State: 6
+Path Length: 20
+States accessed: 294
+Computation Time: 2.0117759704589844ms 
+
+
+State: 7
+Path Length: 14
+States accessed: 104
+Computation Time: 0.9958744049072266ms 
+
+
+State: 8
+Path Length: 26
+States accessed: 12900
+Computation Time: 106.24861717224121ms 
+
+
+State: 9
+Path Length: 22
+States accessed: 7795
+Computation Time: 63.802480697631836ms 
+
+
+State: 10
+Path Length: 31
+States accessed: 40638
+Computation Time: 384.0053081512451ms 
+"""
+
+
+
 def get_path(state, predecessor):
     """
     Given a path specified by  @predecessor dictionary, and a @state
@@ -55,6 +118,15 @@ def move(state):
     return None
 
 def manhattan(state, state1):
+    """
+    Calculates the manhattan distance for each value in @state to its equivalence in @state1, 
+    summing them up to find the total manhattan distance from @state to @state1.
+    
+    param state: state to compare from
+    param state1: state to compare to
+    
+    return: the manhattan distance between @state and @state1
+    """
     by, bx, rows = state
     by1, bx1, rows1 = state1
     
@@ -79,18 +151,16 @@ def manhattan(state, state1):
 
 def IDAstar(state, goal):
     """
-    How to apply Iterative Deepening to A*?
-    1. A pass consists in expanding all nodes that have an value less
-        than or equal to a bound (threshold).
-    2. The initial bound is the value of the start state.
-    3. Expand all nodes that have an value less than or equal to the
-        actual bound (threshold).
-    4. If the goal is not found, update the bound,
-        the next bound is the smallest value of the states generated in
-        the search but not expanded.
-    5. Repeat (do another pass) until the goal is reached.
-    6. When the bound is updated, the search restarts from the start
-        state (ID).
+    IDA* search algorithm to search the tree of possible moves from @state. Uses a heuristic equivalent to the 
+    sum of each nodes depth and its manhattan distance to the goal. Creates a threshold based on the manhattan
+    distance from the @state to the @goal and is updated to the smallest heuristic of the unexplored nodes each 
+    time a search is completed.
+    
+    calls: @IDAstar
+    
+    param case: state from which the search starts
+    param goal: state for the search to find
+    return: the path from @state to @goal and the number of states accessed
     """
     
     
@@ -100,16 +170,17 @@ def IDAstar(state, goal):
     # Initial threshold of the hueristic of @state
     thresh = manhattan(state, goal)
     
+    # Number for the amount of states accessed
     accesses = 0
     
     # Loops as long as thresh is below the maximum moves (g) + maxmimum depth (h)
     while thresh <= maxMoves + maxMoves:
         predecessor = {(state[0], state[1], tuple(tuple(row) for row in state[2])) : None} # Initialises predecessors with hashable state.
         stack = [[0,state]] # Initialises stack with initial state with depth 0.
-        minmanhattan = maxMoves
+        minmanhattan = maxMoves * maxMoves # Reset minmanattan to high number
         while stack:  # While new states still available to search
             
-            depth, current = stack.pop() # Destack most recent unseen element
+            depth, current = stack.pop() # Destack most recent unseen element with depth
             key = current[0], current[1], tuple(tuple(row) for row in current[2]) # Hashable copy of current
             
             if current == goal: #Checks if current is the target state
@@ -118,33 +189,42 @@ def IDAstar(state, goal):
             # Heuristic of the node using depth + manhattan distance to goal
             h = depth + manhattan(current, goal) 
     
-            if h <= thresh: #If heuristic of node is below threshold
-                for next_state in move(current):
-                    accesses += 1
-                    nextKey = next_state[0], next_state[1], tuple(tuple(row) for row in next_state[2])
-                    if nextKey not in predecessor:
-                        stack.append([depth + 1, next_state])
-                        predecessor[nextKey] = key
-            elif h < minmanhattan:
-                minmanhattan = h
+            if h <= thresh: # If heuristic of node is below threshold
+                for next_state in move(current): # For each child / possible move
+                    accesses += 1 # Updates the state accesses number
+                    nextKey = next_state[0], next_state[1], tuple(tuple(row) for row in next_state[2]) # Hashable copy of next_state
+                    if nextKey not in predecessor: # If node is has not been explored
+                        stack.append([depth + 1, next_state]) # Append the next_state with a depth 1 deeper than the parent
+                        predecessor[nextKey] = key # Store next_state with its parent
+            elif h < minmanhattan: # If heuristic of node is above the threshold and below the smallest unexplored child
+                minmanhattan = h # Update minmanhattan to the heurstic of the node
         
-        if minmanhattan != maxMoves: #If minmanhatten has been updated
-            thresh = minmanhattan #Update threshold to minmanhatten
-        else: #If minmanhatten has not been updated
-            thresh += 1 #Increment threshold
+        if minmanhattan != maxMoves * maxMoves: # If minmanhatten has been updated
+            thresh = minmanhattan # Update threshold to minmanhatten
+        else: # If minmanhatten has not been updated
+            thresh += 1 # Increment threshold
     print("No solution found")
-    return [], accesses
+    return [], accesses # Return no path with the number of accesses
 
 
-def main(cases, goal, ind = 0):
-    for i in range(len(cases)):
-        print(f"Case {i + 1 + ind}")
-        start = time.time()
-        path, accesses = IDAstar(cases[i], goal)
-        compTime = time.time() - start
-        print(f"States accessed: {accesses}")
-        print(f"Path Length: {len(path)}")
-        print(f"Computation Time: {compTime * 1000}ms \n\n")
+def solve_puzzle(case, goal):
+    """
+    The main function to call @IDAstar using the given state and goal 
+    while collecting efficiency information and returning it.
+    
+    calls: @IDAstar
+    
+    param case: state from which the search starts
+    param goal: state for the search to find
+    return: number of moves, number of states accessed, computation time and path from case to goal 
+    """
+    start = time.time()
+    path, accesses = IDAstar(case, goal)
+    compTime = time.time() - start
+    print(f"Path Length: {len(path)-1}")
+    print(f"States accessed: {accesses}")
+    print(f"Computation Time: {compTime * 1000}ms \n\n")
+    return (len(path)-1, accesses, compTime, path)
         
 cases1 = [[0, 0, [[0, 7, 1], [4, 3, 2], [8, 6, 5]]],
 [0, 2, [[5, 6, 0], [1, 3, 8], [4, 7, 2]]],
@@ -154,7 +234,12 @@ cases1 = [[0, 0, [[0, 7, 1], [4, 3, 2], [8, 6, 5]]],
 
 goal1 = [0, 2, [[3, 2, 0], [6, 1, 8], [4, 7, 5]]] 
 
-main(cases1, goal1)
+index = 1
+
+for case1 in cases1:
+    print(f"State: {index}")
+    solve_puzzle(case1, goal1)
+    index+=1
 
 cases2 = [[0, 0, [[0, 1, 8], [3, 6, 7], [5, 4, 2]]],
 [2, 0, [[6, 4, 1], [7, 3, 2], [0, 5, 8]]],
@@ -164,4 +249,7 @@ cases2 = [[0, 0, [[0, 1, 8], [3, 6, 7], [5, 4, 2]]],
 
 goal2 = [2, 2, [[1, 2, 3], [4, 5, 6], [7, 8, 0]]]
 
-main(cases2, goal2, 5)
+for case2 in cases2:
+    print(f"State: {index}")
+    solve_puzzle(case2, goal2)
+    index+=1
--- IDDFS.py
+++ IDDFS.py
@@ -1,5 +1,66 @@
 import time
 
+"""
+State: 1
+Path Length: 22
+States accessed: 79228
+Computation Time: 368.0005073547363ms 
+
+
+State: 2
+Path Length: 24
+States accessed: 122164
+Computation Time: 624.0708827972412ms 
+
+
+State: 3
+Path Length: 40
+States accessed: 1684010
+Computation Time: 8737.18810081482ms 
+
+
+State: 4
+Path Length: 34
+States accessed: 1272873
+Computation Time: 6551.671981811523ms 
+
+
+State: 5
+Path Length: 22
+States accessed: 350602
+Computation Time: 1773.611068725586ms 
+
+
+State: 6
+Path Length: 28
+States accessed: 278142
+Computation Time: 1366.4963245391846ms 
+
+
+State: 7
+Path Length: 14
+States accessed: 22140
+Computation Time: 132.60436058044434ms 
+
+
+State: 8
+Path Length: 38
+States accessed: 1852312
+Computation Time: 9567.428350448608ms 
+
+
+State: 9
+Path Length: 22
+States accessed: 125813
+Computation Time: 577.1675109863281ms 
+
+
+State: 10
+Path Length: 35
+States accessed: 988424
+Computation Time: 5134.518146514893ms 
+"""
+
 def get_path(state, predecessor):
     """
     Given a path specified by  @predecessor dictionary, and a @state
@@ -22,31 +83,31 @@ def move(state):
     generator that returns all states that are reached from
     state, in one move.
 
-    :param state:
+    :param state: a list containing the blank position (x,y) and the grid of values
     :return: next_state
     """
     by, bx, rows = state
     
-    if bx < len(rows[0])-1: #Blank space not on right
-        #Move Right
+    if bx < len(rows[0])-1: # Blank space not on right
+         #Move Blank Right
         temp_rows = [list(row) for row in rows]
         temp_rows[by][bx], temp_rows[by][bx+1] = temp_rows[by][bx+1], 0
         next_state = [by, bx+1, temp_rows]
         yield next_state
-    if bx > 0: #Blank space not on left
-        #Move Left
+    if bx > 0: # Blank space not on left
+         #Move Blank Left
         temp_rows = [list(row) for row in rows]
         temp_rows[by][bx], temp_rows[by][bx-1] = temp_rows[by][bx-1], 0
         next_state = [by, bx-1, temp_rows]
         yield next_state
-    if by < len(rows)-1: #Blank space not at bottom
-        #Move Down
+    if by < len(rows)-1: # Blank space not at bottom
+        # Move Blank Down
         temp_rows = [list(row) for row in rows]
         temp_rows[by][bx], temp_rows[by+1][bx] = temp_rows[by+1][bx], 0
         next_state = [by+1, bx, temp_rows]
         yield next_state
-    if by > 0: #Blank space not at top
-        #Move Up
+    if by > 0: # Blank space not at top
+        # Move Blank Up
         temp_rows = [list(row) for row in rows]
         temp_rows[by][bx], temp_rows[by-1][bx]  = temp_rows[by-1][bx], 0
         next_state = [by-1, bx, temp_rows]
@@ -59,52 +120,64 @@ def iddfs(state, goal):
     """
     NON-RECURSIVE version of the depth first search of a cyclic search space.
     uses a stack of states, @state, and a dictionary @predecessor to store the predecessor of each state along the
-    search path.
+    search path. Searches the entire tree using DFS until the search depth is reach, then increments the search depth 
+    by 1 each time the search is complete.
 
     calls @get_path
-    calls @is_goal
 
     Note: the dictionary is initialized by the statement:  predecessor = {state : None}. "None" is a marker
     to identify the starting state when reconstruction a path by @get_path
     :param state: state from which the search starts
-    :return: path to the goal state
+    :param goal: state for the search to find
+    :return: path to the goal state and number of states accessed
     """
     
+    # Each number multiplied by maximum manhatten (across and up/down) for each number
     maxMoves = (len(state[2])*len(state[2][0]) * (len(state[2])-1 + len(state[2][0])-1))+1
     
+    # Number for the amount of states accessed
     accesses = 0
     
     for search_depth in range(maxMoves + maxMoves):
         predecessor = {(state[0], state[1], tuple(tuple(row) for row in state[2])) : None}
         stack = [[0,state]]
-        while stack:
+        while stack: # While new states still available to search
             
-            depth, current = stack.pop()
-            key = current[0], current[1], tuple(tuple(row) for row in current[2])
+            depth, current = stack.pop() # Destack most recent unseen element with depth
+            key = current[0], current[1], tuple(tuple(row) for row in current[2]) # Hashable copy of current
                 
-            if current == goal:
-                return get_path(key, predecessor), accesses
+            if current == goal: # Checks if current is the target state
+                return get_path(key, predecessor), accesses # Returns path from inital state to target state and the number of accesses.
 
-            if depth <= search_depth:
-                for next_state in move(current):
-                    accesses += 1
-                    nextKey = next_state[0], next_state[1], tuple(tuple(row) for row in next_state[2])
-                    if nextKey not in predecessor:
-                        stack.append([depth + 1, next_state])
-                        predecessor[nextKey] = key
+            if depth <= search_depth: # If depth of current node is higher than the search depth
+                for next_state in move(current): # For each child / possible move
+                    accesses += 1 # Updates the state accesses number
+                    nextKey = next_state[0], next_state[1], tuple(tuple(row) for row in next_state[2]) # Hashable copy of next_state
+                    if nextKey not in predecessor: # If node is has not been explored
+                        stack.append([depth + 1, next_state]) # Append the next_state with a depth 1 deeper than the parent
+                        predecessor[nextKey] = key # Store next_state with its parent
                     
     print("No solution found")
     return [], accesses
 
-def main(cases, goal, ind = 0):
-    for i in range(len(cases)):
-        print(f"Case {i + 1 + ind}")
-        start = time.time()
-        path, accesses = iddfs(cases[i], goal)
-        compTime = time.time() - start
-        print(f"States accessed: {accesses}")
-        print(f"Path Length: {len(path)}")
-        print(f"Computation Time: {compTime * 1000}ms \n\n")
+def solve_puzzle(case, goal):
+    """
+    The main function to call @iddfs using the given state and goal 
+    while collecting efficiency information and returning it.
+    
+    calls: @iddfs
+    
+    param case: state from which the search starts
+    param goal: state for the search to find
+    return: number of moves, number of states accessed, computation time and path from case to goal 
+    """
+    start = time.time()
+    path, accesses = iddfs(case, goal)
+    compTime = time.time() - start
+    print(f"Path Length: {len(path)-1}")
+    print(f"States accessed: {accesses}")
+    print(f"Computation Time: {compTime * 1000}ms \n\n")
+    return (len(path)-1, accesses, compTime, path)
         
 cases1 = [[0, 0, [[0, 7, 1], [4, 3, 2], [8, 6, 5]]],
 [0, 2, [[5, 6, 0], [1, 3, 8], [4, 7, 2]]],
@@ -114,7 +187,12 @@ cases1 = [[0, 0, [[0, 7, 1], [4, 3, 2], [8, 6, 5]]],
 
 goal1 = [0, 2, [[3, 2, 0], [6, 1, 8], [4, 7, 5]]] 
 
-main(cases1, goal1)
+index = 1
+
+for case1 in cases1:
+    print(f"State: {index}")
+    solve_puzzle(case1, goal1)
+    index+=1
 
 cases2 = [[0, 0, [[0, 1, 8], [3, 6, 7], [5, 4, 2]]],
 [2, 0, [[6, 4, 1], [7, 3, 2], [0, 5, 8]]],
@@ -124,4 +202,7 @@ cases2 = [[0, 0, [[0, 1, 8], [3, 6, 7], [5, 4, 2]]],
 
 goal2 = [2, 2, [[1, 2, 3], [4, 5, 6], [7, 8, 0]]]
 
-main(cases2, goal2, 5)
+for case2 in cases2:
+    print(f"State: {index}")
+    solve_puzzle(case2, goal2)
+    index+=1
